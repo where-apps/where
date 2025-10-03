5b4707eee92b1f312c392e1f9afc6a52b4302a01
primeryan <217597218+primeryan@users.noreply.github.com>
2025-10-03 12:39:20 +0100

    Update trpc.ts

--- where/lib/trpc.ts
+++ where/lib/trpc.ts
@@ -9,10 +9,7 @@ const getBaseUrl = () => {
   if (process.env.EXPO_PUBLIC_RORK_API_BASE_URL) {
     return process.env.EXPO_PUBLIC_RORK_API_BASE_URL;
   }
-
-  throw new Error(
-    "No base url found, please set EXPO_PUBLIC_RORK_API_BASE_URL"
-  );
+  return "https://where-1.onrender.com";
 };
 
 export const trpcClient = trpc.createClient({
