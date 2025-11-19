39ddcc83c340c6082864262c5541f97d3a0090fb
primeryan <217597218+primeryan@users.noreply.github.com>
2025-11-19 18:08:54 +0100

    Update route.ts

--- where/backend/trpc/routes/locations/get/route.ts
+++ where/backend/trpc/routes/locations/get/route.ts
@@ -3,9 +3,11 @@ import { publicProcedure } from "@/backend/trpc/create-context";
 
 const S5_BASE_URL = process.env.S5_BASE_URL ?? "https://where-app.com";
 
+const getLocationInput = z.object({ cid: z.string() });
+
 export default publicProcedure
-  .input(z.object({ cid: z.string() }))
-  .query(async ({ input }) => {
+  .input(getLocationInput)
+  .query(async ({ input }: { input: z.infer<typeof getLocationInput> }) => {
     const res = await fetch(`${S5_BASE_URL}/s5/gateway/${input.cid}`);
     
     if (!res.ok) {
