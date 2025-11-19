d1436b7b934ac302dd1a0438d9d6a34c63b48e1c
primeryan <217597218+primeryan@users.noreply.github.com>
2025-11-19 16:11:51 +0100

    Update app-router.ts

--- where/backend/trpc/app-router.ts
+++ where/backend/trpc/app-router.ts
@@ -1,9 +1,23 @@
 import { createTRPCRouter } from "./create-context";
 import hiRoute from "./routes/example/hi/route";
+import createLocationRoute from "./routes/locations/create/route";
+import listLocationsRoute from "./routes/locations/list/route";
+import getLocationRoute from "./routes/locations/get/route";
+import updateLocationRoute from "./routes/locations/update/route";
+import addCommentRoute from "./routes/comments/add/route";
 
 export const appRouter = createTRPCRouter({
   example: createTRPCRouter({
     hi: hiRoute,
+  }),
+  locations: createTRPCRouter({
+    create: createLocationRoute,
+    list: listLocationsRoute,
+    get: getLocationRoute,
+    update: updateLocationRoute,
+  }),
+  comments: createTRPCRouter({
+    add: addCommentRoute,
   }),
 });
 
